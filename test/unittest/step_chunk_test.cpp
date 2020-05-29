7b28631889f55ad625ef29231b7ef7b8b71d55a3
Albin Johansson <albinoscar99@hotmail.com>
2020-05-29 17:40:42 +0200

    Add Chunk class

---
+++ test/unittest/step_chunk_test.cpp
@@ -0,0 +1,44 @@
+#include "step_chunk.h"
+
+#include <doctest.h>
+
+#include "step_test_utils.h"
+
+using namespace step;
+
+inline const std::string prefix = "resource/chunk/";
+
+TEST_SUITE("Chunk")
+{
+  TEST_CASE("Parse chunk with CSV data")
+  {
+    const auto chunk = test::make<Chunk>(prefix, "chunk_csv.json");
+
+    SUBCASE("Test obtaining data")
+    {
+      const auto& data = chunk.data();
+      CHECK_NOTHROW(data.data_gid());
+      CHECK_THROWS(data.data_base64());
+    }
+    CHECK(chunk.x() == 7);
+    CHECK(chunk.y() == -18);
+    CHECK(chunk.width() == 16);
+    CHECK(chunk.height() == 22);
+  }
+
+  TEST_CASE("Parse chunk with Base64 data")
+  {
+    const auto chunk = test::make<Chunk>(prefix, "chunk_base64.json");
+
+    SUBCASE("Test obtaining data")
+    {
+      const auto& data = chunk.data();
+      CHECK_NOTHROW(data.data_base64());
+      CHECK_THROWS(data.data_gid());
+    }
+    CHECK(chunk.x() == 45);
+    CHECK(chunk.y() == 77);
+    CHECK(chunk.width() == 31);
+    CHECK(chunk.height() == 29);
+  }
+}
