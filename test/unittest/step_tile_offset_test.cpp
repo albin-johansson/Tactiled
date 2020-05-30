e436320040f1e36ab5592d540ee11b16b2f6880a
Albin Johansson <albinoscar99@hotmail.com>
2020-05-30 17:02:42 +0200

    Update step_tile_offset_test.cpp

--- test/unittest/step_tile_offset_test.cpp
+++ test/unittest/step_tile_offset_test.cpp
@@ -2,27 +2,17 @@
 
 #include <doctest.h>
 
-#include "step_utils.h"
+#include "step_test_utils.h"
 
 using namespace step;
 
-namespace {
-
-TileOffset mk_tile_offset(std::string_view path)
-{
-  using namespace std::string_literals;
-  const std::string actualPath = "resource/tileoffset/"s + path.data();
-  const auto json = detail::parse_json(actualPath.c_str());
-  return json.get<TileOffset>();
-}
-
-}  // namespace
+inline static const std::string prefix = "resource/tileoffset/";
 
 TEST_SUITE("TileOffset")
 {
   TEST_CASE("Valid tile offset")
   {
-    const auto offset = mk_tile_offset("tile_offset.json");
+    const auto offset = test::make<TileOffset>(prefix, "tile_offset.json");
     CHECK(offset.x() == 123);
     CHECK(offset.y() == 829);
   }
