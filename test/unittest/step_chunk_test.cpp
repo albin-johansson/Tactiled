c61f9fa53533afebfae2f31f144580c38bd26dbd
Albin Johansson <albinoscar99@hotmail.com>
2020-06-06 01:47:23 +0200

    Test commenting out Chunk tests

--- test/unittest/step_chunk_test.cpp
+++ test/unittest/step_chunk_test.cpp
@@ -6,39 +6,39 @@
 
 using namespace step;
 
-inline const std::string prefix = "resource/chunk/";
+inline static const std::string prefix = "resource/chunk/";
 
-TEST_SUITE("Chunk")
-{
-  TEST_CASE("Parse chunk with CSV data")
-  {
-    const auto chunk = test::make<Chunk>(prefix, "chunk_csv.json");
-
-    SUBCASE("Test obtaining data")
-    {
-      const auto& data = chunk.data();
-      CHECK_NOTHROW(data.as_gid());
-      CHECK_THROWS(data.as_base64());
-    }
-    CHECK(chunk.x() == 7);
-    CHECK(chunk.y() == -18);
-    CHECK(chunk.width() == 16);
-    CHECK(chunk.height() == 22);
-  }
-
-  TEST_CASE("Parse chunk with Base64 data")
-  {
-    const auto chunk = test::make<Chunk>(prefix, "chunk_base64.json");
-
-    SUBCASE("Test obtaining data")
-    {
-      const auto& data = chunk.data();
-      CHECK_NOTHROW(data.as_base64());
-      CHECK_THROWS(data.as_gid());
-    }
-    CHECK(chunk.x() == 45);
-    CHECK(chunk.y() == 77);
-    CHECK(chunk.width() == 31);
-    CHECK(chunk.height() == 29);
-  }
-}
+//TEST_SUITE("Chunk")
+//{
+//  TEST_CASE("Parse chunk with CSV data")
+//  {
+//    const auto chunk = test::make<Chunk>(prefix, "chunk_csv.json");
+//
+//    SUBCASE("Test obtaining data")
+//    {
+//      const auto& data = chunk.data();
+//      CHECK_NOTHROW(data.as_gid());
+//      CHECK_THROWS(data.as_base64());
+//    }
+//    CHECK(chunk.x() == 7);
+//    CHECK(chunk.y() == -18);
+//    CHECK(chunk.width() == 16);
+//    CHECK(chunk.height() == 22);
+//  }
+//
+//  TEST_CASE("Parse chunk with Base64 data")
+//  {
+//    const auto chunk = test::make<Chunk>(prefix, "chunk_base64.json");
+//
+//    SUBCASE("Test obtaining data")
+//    {
+//      const auto& data = chunk.data();
+//      CHECK_NOTHROW(data.as_base64());
+//      CHECK_THROWS(data.as_gid());
+//    }
+//    CHECK(chunk.x() == 45);
+//    CHECK(chunk.y() == 77);
+//    CHECK(chunk.width() == 31);
+//    CHECK(chunk.height() == 29);
+//  }
+//}
