5791be25f926092764ff76fd2abf407224fa6a30
Albin Johansson <albinoscar99@hotmail.com>
2020-06-10 16:46:29 +0200

    Update step_test_utils.h

--- test/unittest/step_test_utils.h
+++ test/unittest/step_test_utils.h
@@ -9,8 +9,7 @@
 namespace step::test {
 
 template <typename T>
-T make(const std::string& prefix,
-       std::string_view file)  // TODO prefix string_view
+T make(const std::string& prefix, std::string_view file)
 {
   const auto json = detail::parse_json(prefix + file.data());
   return json.get<T>();
