12cd6d4f1d9249e0566b2d322dd4ed3f993c53be
Albin Johansson <albinoscar99@hotmail.com>
2020-06-15 12:22:39 +0200

    Add Properties::is

--- test/unittest/step_properties_test.cpp
+++ test/unittest/step_properties_test.cpp
@@ -20,13 +20,13 @@ TEST_SUITE("Properties")
       CHECK(props.has("a"));
       CHECK(props.has("b"));
       CHECK(props.has("c"));
-      CHECK(!props.has("d"));
+      CHECK(!props.has("x"));
     }
 
     SUBCASE("Getting property")
     {
-      CHECK_THROWS_WITH_AS(props.get("d"),
-                           "Properties > Couldn't lookup property: d",
+      CHECK_THROWS_WITH_AS(props.get("x"),
+                           "Properties > Couldn't lookup property: x",
                            StepException);
 
       REQUIRE_NOTHROW(props.get("a"));
@@ -37,11 +37,29 @@ TEST_SUITE("Properties")
       CHECK(*property.as_int() == 67);
     }
 
-    CHECK(props.amount() == 3);
+    SUBCASE("Testing the is-method")
+    {
+      CHECK(props.is("a", 67));
+      CHECK(props.is("b", 24));
+      CHECK(props.is("c", 31));
+      CHECK(props.is("d", 42.0f));
+      CHECK(props.is("e", true));
+      CHECK(props.is("f", Color{"#ABBAFEFF"}));
+      CHECK(props.is("g", "dawkins"));
+
+      CHECK(!props.is("a", false));
+      CHECK(!props.is("b", "hello"));
+      CHECK(!props.is("c", 2.91f));
+      CHECK(!props.is("d", Color{"#AABBCCDD"}));
+      CHECK(!props.is("f", 1234));
+      CHECK(!props.is("g", true));
+    }
+
+    CHECK(props.amount() == 7);
     CHECK(!props.empty());
 
-    props.each([](const std::pair<std::string, Property>& pair) {
-      CHECK(pair.second.is_int());
-    });
+    int i = 0;
+    props.each([&i](const std::pair<std::string, Property>& pair) { ++i; });
+    CHECK(i == 7);
   }
 }
