8fa9f97e4fa41cde938a8ae7b278a0a40c82c677
Albin Johansson <albinoscar99@hotmail.com>
2020-09-21 19:18:19 +0200

    Update step_grid_test.cpp

--- test/unittest/step_grid_test.cpp
+++ test/unittest/step_grid_test.cpp
@@ -2,15 +2,15 @@
 
 #include <doctest.h>
 
-#include "step_test_utils.h"
-
-inline static const std::string prefix = "resource/grid/";
+#include "step_utils.hpp"
 
 TEST_SUITE("grid")
 {
   TEST_CASE("Load valid isometric grid")
   {
-    const auto grid = step::test::make<step::grid>(prefix, "isometric.json");
+    const step::grid grid{
+        step::detail::parse_json("resource/grid/isometric.json")};
+
     CHECK(grid.get_orientation() == step::grid::orientation::isometric);
     CHECK(grid.width() == 18);
     CHECK(grid.height() == 24);
@@ -18,7 +18,8 @@ TEST_SUITE("grid")
 
   TEST_CASE("Load valid orthogonal grid")
   {
-    const auto grid = step::test::make<step::grid>(prefix, "orthogonal.json");
+    const step::grid grid{
+        step::detail::parse_json("resource/grid/orthogonal.json")};
     CHECK(grid.get_orientation() == step::grid::orientation::orthogonal);
     CHECK(grid.width() == 54);
     CHECK(grid.height() == 28);
